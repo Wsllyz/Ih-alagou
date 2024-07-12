efcf506a309f535fb6c3248cad1c3619641cd311
Weslley Santana <westvsantana@gmail.com>
2024-07-12 00:10:39 -0300

    updateProject

--- ihAlagou/home/views.py
+++ ihAlagou/home/views.py
@@ -7,10 +7,16 @@ def pegaDistancia(dado):
     return {'distância': dado['payload']}
 
 # Create your views 
+def distancia_view(request):
+    items = get_items()
+    ultimoItem = items[-1]
+    distancia = pegaDistancia(ultimoItem)
+    distancia = distancia['distância']
+    return JsonResponse({'distância': distancia['distância']})
+
 def home_view(request):
     items = get_items()
     ultimoItem = items[-1]
     distancia = pegaDistancia(ultimoItem)
     distancia = distancia['distância']
-    distancia = distancia['distância']
-    return render(request, 'index.html', {'distância':distancia})
+    return render(request, 'index.html', {'distância':distancia['distância']})
