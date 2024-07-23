90e4bf812c44b38ccbf0ffe31dfdb8e4ed5db489
Weslley Santana <westvsantana@gmail.com>
2024-07-23 00:04:27 -0300

    updateProject

--- ihAlagou/home/views.py
+++ ihAlagou/home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import requests
+
 
 
 def pegaDistancia(dado):
@@ -15,14 +17,15 @@ def distancia_view(request):
 '''
 
 def distancia_view(request):
-    pass
-    return '2'
+    response = requests.get('https://api.thingspeak.com/channels/2602561/feeds.json?api_key=4OW0Z91BKJ5U61ZT&results=2')
+    if response.status_code == 200:
+        dados = response.json()
+        return JsonResponse({'dados':dados})
+    return
 
 def home_view(request):
-    '''
-    items = get_items()
-    ultimoItem = items[-1]
-    distancia = pegaDistancia(ultimoItem)
-    distancia = distancia['distância']
-    '''
-    return render(request, 'index.html')
+    response = requests.get('https://api.thingspeak.com/channels/2602561/feeds.json?api_key=4OW0Z91BKJ5U61ZT&results=2')
+    if response.status_code == 200:
+        dados = response.json()
+        return render(request, 'index.html', {'dados':dados})
+    return render(request, 'index.html', {'dados':dados})
